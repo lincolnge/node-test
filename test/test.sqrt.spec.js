be952e0cdf18b78c0a64981f44d537e3bc1d6370
王耕洲 <wanggengzhou@meituan.com>
2016-12-21 19:55:37 +0800

    Add: add istanbul with spec test.

---
+++ test/test.sqrt.spec.js
@@ -0,0 +1,33 @@
+// ╰─$ istanbul cover _mocha test/test.sqrt.spec.js
+
+var assert = require('assert');
+var My = require('../src/sqrt.js');
+// var sim = require('../src/simple.js')
+
+describe("assert sqrt", function() {
+
+  it("4的平方根应该等于", function() {
+    assert.equal(My.sqrt(4), 2);
+  });
+
+  it("参数为负值时应该报错", function() {
+    assert.doesNotThrow(
+      function() {
+        My.sqrt(3);
+      },
+      '参数为负值时应该报错'
+    );
+    assert.throws(
+      function() {
+        My.sqrt(-1);
+      },
+      function(err) {
+        // console.info('err', err);
+        if ( (err instanceof Error) && /负值没有平方根/.test(err) ) {
+          return true;
+        }
+      },
+      '参数为负值时应该报错'
+    );
+  });
+});
