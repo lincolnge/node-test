6b407f2719a67c79df18686f28e285ce50012451
王耕洲 <wanggengzhou@meituan.com>
2016-12-22 16:15:01 +0800

    Add: update readme.

--- test/test.privacy.spec.js
+++ test/test.privacy.spec.js
@@ -1,5 +1,9 @@
 // $ mocha test/test.privacy.spec.js
 
+/*
+ * 每一个被rewire引入的模块都有__set__()和__get__()方法。它巧妙地利用了闭包的诀窍，
+ * 在eval()执行时，实现了对模块内部局部变量的访问，从而可以将局部变量导出给测试用例调用执行。
+ */
 describe('Test privacy function', function() {
   it('should without error', function() {
     var rewire = require('rewire');
